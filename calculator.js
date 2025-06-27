6f3e97a350ce14040784b45f8911e72254a7213d
Joelle Perez <49221382+JLionPerez@users.noreply.github.com>
2025-06-26 23:25:35 -0700

    Add js version of setting up calculator

--- calculator.js
+++ calculator.js
@@ -1,13 +1,93 @@
+/* ----------------------------------- HTML Setup -----------------------------------*/
 const calculator = document.querySelector(".calculator");
 
 const display = document.createElement("div");
 display.setAttribute("class", "display");
 calculator.appendChild(display);
 
-const numbers = document.createElement("div");
-numbers.setAttribute("class", "numbers");
-calculator.appendChild(numbers);
+const buttons = document.createElement("divs");
+buttons.setAttribute("class", "buttons");
 
-const operators = document.createElement("div");
-operators.setAttribute("class", "operators");
-calculator.appendChild(operators);
+let populateButtons = (buttons) => {
+    let textArr =[
+        1,2,3,'+',
+        4,5,6,'-',
+        7,8,9,'*',
+        'C',0,'=','/'
+    ]
+
+    let i = 0;
+    for (let child of buttons.children) {
+        for (let grandchild of child.children) {
+            grandchild.textContent = textArr[i]
+            i++;
+        }
+    }
+}
+
+let createButtons = (length) => {
+    for (let i = 0; i < length; i++) {
+        const row = document.createElement("div")
+        row.setAttribute("class", "row")
+        for (let j = 0; j < length; j++) {
+            const button = document.createElement("button")
+            row.appendChild(button)
+        }
+        buttons.appendChild(row)
+        populateButtons(buttons)
+    }
+    calculator.appendChild(buttons)
+}
+
+createButtons(4);
+
+/* ------------------------------- Operator Functions -------------------------------*/
+let add = (x, y) => {
+    return x + y
+}
+
+let subtract = (x, y) => {
+    return x - y
+}
+
+let multiply = (x, y) => {
+    return x * y
+}
+
+let divide = (x, y) => {
+    return x / y
+}
+
+let displayInput = (obj) => {
+    obj.addEventListener("click", (event) => {
+        display.textContent = obj.textContent
+        calculator.appendChild(display);
+    })
+}
+
+// displayInput(buttons)
+
+let operate = (first, operator, second) => {
+    let result;
+    if (operator === '+') {
+        result = add(first, second)
+    }
+    if (operator === ('-')) {
+        result = subtract(first, second)
+    }
+    if (operator === ('*')) {
+        result = multiply(first, second)
+    }
+    if (operator === ('/')) {
+        result = divide(first, second)
+    }
+    if (first === ('=')) { return result }
+}
+
+//tests
+console.log(operate(7,'+',3))
+console.log(operate(7,'-',3))
+console.log(operate(7,'*',3))
+console.log(operate(21,'/',3))
+console.log(operate(21,'+',3,'/'))
+// console.log('=')
